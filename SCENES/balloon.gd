cf594a04c8ecf73978374dff6be703de731a57b8
Mira Santa Lucia <santaluciamira@gmail.com>
2022-04-30 22:21:52 -0700

    Visually polished Helium levels and Balloons

--- SCENES/balloon.gd
+++ SCENES/balloon.gd
@@ -1,9 +1,13 @@
 extends KinematicBody2D
 
+const MAX_FLOAT = 10.0
+
 export var start_position = Vector2(0,0)
 export var end_position = Vector2(0,0)
 export var duration = 1
-export var type = 1 # 1 for blue, 2 for green (w/ spikes)
+export var type = 1 # 1 for green, 2 for red (w/ spikes)
+
+var velocity = Vector2.ZERO
 
 onready var tween_positions = [start_position, end_position]
 
@@ -28,3 +32,21 @@ func _on_Tween_tween_completed(object, key):
 	tween_positions.invert()
 	start_tween()
 
+
+func _physics_process(delta):
+	var v_amp = MAX_FLOAT / abs(start_position.y - end_position.y + 1)
+	velocity = Vector2(0, get_cos_height($FloatTimer.time_left, 
+			$FloatTimer.wait_time, v_amp))
+	$AnimatedSprite.position.y = velocity.y
+
+
+# Returns the y-value of point x on a cosine graph of defined transformation
+# (this is literally exactly the same as seen in sidekick.gd):
+# 	param x - x-value of point
+# 	param period - length of one complete cycle of the cosine graph
+# 	param amplitude - amplitude (maximum or |minumum|) of graph
+# 	return float y-value of the point that corresponds with given x-value
+func get_cos_height(var x, var period, var amplitude):
+	var frequency = 2*PI / period
+#	print(amplitude * cos(frequency * x))
+	return amplitude * cos(frequency * x)
